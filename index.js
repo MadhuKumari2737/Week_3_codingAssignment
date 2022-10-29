0a2147622db7eca4939dc00244813f1abd21d400
Madhu Kumari <madhukumari2737@gmail.com>
2022-10-28 22:33:28 -0400

    coding_assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+var itemPrice = 49.99;
+var amountOfMoneyinWallet = 76;
+var NumberOfFriends = 6;
+var ageInYears = 30;
+var firstName = "Madhu";
+var lastName = "Kumari";
+var middleinitial = "P"
+ 
+var newAmountOfMoney = (amountOfMoneyinWallet - itemPrice);
+console.log(newAmountOfMoney);
+
+
+var fullName = (firstName + middleinitial + lastName);
+console.log(fullName);

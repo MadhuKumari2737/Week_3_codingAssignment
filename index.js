dd13df127e0b1e357fc4a6630fae404cc72eaf85
Madhu Kumari <madhukumari2737@gmail.com>
2022-10-28 23:00:47 -0400

    modified based on assignment

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var itemPrice = 49.99;
+var itemPrice = 49.50;
 var amountOfMoneyinWallet = 76;
-var NumberOfFriends = 6;
+var numberOfFriends = 6;
 var ageInYears = 30;
 var firstName = "Madhu";
 var lastName = "Kumari";
 var middleinitial = "P"
  
 var newAmountOfMoney = (amountOfMoneyinWallet - itemPrice);
-console.log(newAmountOfMoney);
+console.log('My new amount of money: ' + newAmountOfMoney);
+
+var friendsEachYear = (numberOfFriends / ageInYears);
+console.log('Friends per year: ' + friendsEachYear)
 
 
-var fullName = (firstName + middleinitial + lastName);
-console.log(fullName);
+var fullName = (firstName + " " + middleinitial + " " + lastName);
+console.log('My full name: ' + fullName);
